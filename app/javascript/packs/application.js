6fce9821962d81f847014587efa68dc9dfe63573
Mickael Taillepierre <mick0987@hotmail.ch>
2020-08-14 09:48:56 +0200

    front

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,6 +21,3 @@ import 'bootstrap';
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-$(".yourelement").typed
-string: ["Typed.js is a <strong>jQuery</strong> plugin."]
-contentType: 'html'
